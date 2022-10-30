7b7ad8e1e71c5a879fce006fee6b68d7824cfee9
Peter Lin <zili329@vcs.email>
2022-10-30 10:44:56 -0700

    Final Change for Deck

--- src/main/java/u3pp/Main.java
+++ src/main/java/u3pp/Main.java
@@ -1,11 +1,21 @@
 package u3pp;
-
+import u3pp.Deck;
 public class Main 
 {
+
+
+
+
+
+
     public static void main(String[] args) {
-        
-        // Use this space to test your code, or actually run your project
-        System.out.println("\n\nHi! This is an empty project.\nYour code will start running from Main.java.\nIt may be daunting to start a project from scratch, but try to have fun with it! :)\n");
+        Deck d = new Deck();
+        d.deal();
+        d.deal();
+        d.deal();
+        d.deal();
+        d.deal();
+
 
     }
 
